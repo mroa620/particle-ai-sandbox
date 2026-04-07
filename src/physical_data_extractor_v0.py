31bac343cd50249d1b5a260fd40b3e126b3ac80e
mroa620 <mroa620@gmail.com>
2026-04-07 10:08:41 +0200

    Refactor: unificación de proyectos y actualización de rutas con pathlib

--- src/physical_data_extractor_v0.py
+++ src/physical_data_extractor_v0.py
@@ -173,11 +173,14 @@ CONFIG = {
 
 # [BLOQUE DE ORQUESTACIÓN]
 if __name__ == "__main__":
-    base_dir = Path(__file__).resolve().parent
+    base_dir = Path(__file__).resolve().parent.parent
 
     # 1. Definimos las rutas
-    entrada = base_dir / "simulacion_particulas.csv"
-    salida = base_dir / "datos_limpios.json"
+    entrada = base_dir / "data" / "simulacion_particulas.csv"
+    salida = base_dir / "data" / "datos_limpios.json"
+
+    print(f"Buscando entrada en: {entrada}")
+    print(f"Guardando salida en: {salida}")
 
     print("--- Iniciando Physical Data Extractor v0 ---")
 
